7f78c810d5748aa35bafa26bffcc032f4ef73781
Abdelrahman Adel <abdelrahmanadel203@gmail.com>
2022-11-17 00:09:33 +0200

    Test Commit

--- OOP/Assignment1/Assignment/main.cpp
+++ OOP/Assignment1/Assignment/main.cpp
@@ -1,7 +1,7 @@
 #include "header.h"
 int main(){
-    BigDecimalInt num2("-0056"); 
-    BigDecimalInt num1("-2443"); 
-    BigDecimalInt num3 = num2 - num1; 
+    BigDecimalInt num2("0000000000000"); 
+    BigDecimalInt num1("+2443"); 
+    BigDecimalInt num3 = num2 + num1; 
     cout<<num3;
 }
